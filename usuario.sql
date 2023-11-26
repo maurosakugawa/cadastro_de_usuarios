f524ee566967301d024d753922ae156fee967be1
Mauro Toshiuki Sakugawa <43210185+maurosakugawa@users.noreply.github.com>
2023-11-25 22:32:31 -0300

    Adicionado usuario.sql
    Esse arquivo deve ser importado para seu banco de dados para criar a tabela usuario

---
+++ usuario.sql
@@ -0,0 +1,74 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1:3306
+-- Tempo de geração: 25/11/2023 às 21:55
+-- Versão do servidor: 10.6.15-MariaDB-cll-lve
+-- Versão do PHP: 7.2.34
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `u322340543_CDE`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `usuario`
+--
+
+CREATE TABLE `usuario` (
+  `id_usuario` int(11) NOT NULL,
+  `nome` varchar(80) CHARACTER SET utf8mb3 COLLATE utf8mb3_bin NOT NULL,
+  `sobrenome` varchar(90) CHARACTER SET utf8mb3 COLLATE utf8mb3_bin NOT NULL,
+  `email` varchar(100) CHARACTER SET utf8mb3 COLLATE utf8mb3_bin NOT NULL,
+  `senha` varchar(256) CHARACTER SET utf8mb3 COLLATE utf8mb3_bin NOT NULL,
+  `nivel_usuario` int(2) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1 COLLATE=latin1_swedish_ci;
+
+--
+-- Despejando dados para a tabela `usuario`
+--
+
+INSERT INTO `usuario` (`id_usuario`, `nome`, `sobrenome`, `email`, `senha`, `nivel_usuario`) VALUES
+(6, 'J?lia', 'Santos', 'js@email.com', '7c4a8d09ca3762af61e59520943dc26494f8941b', 1),
+(25, 'M?rio', 'Paulino', 'mp@email.com', '7c4a8d09ca3762af61e59520943dc26494f8941b', 2),
+(43, 'teste', 'teste', 'tt@email.com', '7c4a8d09ca3762af61e59520943dc26494f8941b', 1),
+(18, 'Maria', 'Clara', 'mc@email.com', '7c4a8d09ca3762af61e59520943dc26494f8941b', 1),
+(24, 'Augusto', 'C?sar', 'ac@email.com', '7c4a8d09ca3762af61e59520943dc26494f8941b', 1),
+(42, 'Pedro', 'Paiva', 'pp@email.com', '7c4a8d09ca3762af61e59520943dc26494f8941b', 1);
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices de tabela `usuario`
+--
+ALTER TABLE `usuario`
+  ADD PRIMARY KEY (`id_usuario`);
+
+--
+-- AUTO_INCREMENT para tabelas despejadas
+--
+
+--
+-- AUTO_INCREMENT de tabela `usuario`
+--
+ALTER TABLE `usuario`
+  MODIFY `id_usuario` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=44;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;

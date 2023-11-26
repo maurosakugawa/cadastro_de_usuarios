32230aa87549904d0d97ed419c190a2984112927
Mauro Toshiuki Sakugawa <43210185+maurosakugawa@users.noreply.github.com>
2023-11-25 22:25:29 -0300

    Add files via upload
    Adicionado head.php

---
+++ head.php
@@ -0,0 +1,16 @@
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <!-- links de importação de css -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
+    <link rel="stylesheet" href="css/style.css">
+    <!-- links de importação de javasceipt -->
+    <script src="https://code.jquery.com/jquery-3.6.4.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://kit.fontawesome.com/15b4b8b90e.js"></script>
+    <base href="https://www.valedigitalservice.com.br/">
+</head>

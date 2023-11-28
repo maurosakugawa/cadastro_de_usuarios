f8e610f2e3ade6d70cff661f39ff30d074bdce75
Mauro Toshiuki Sakugawa <43210185+maurosakugawa@users.noreply.github.com>
2023-11-28 18:31:09 -0300

    Update excluir_usuario.php
    Modifica o nome do modal de modal title para exclusão de usuário

--- excluir_usuario.php
+++ excluir_usuario.php
@@ -28,7 +28,7 @@ $usuario = mysqli_fetch_array($resultado);
                 <div class="modal-content">
                     <div class="modal-header">
                         <h5 class="modal-title">
-                            Modal title
+                            EXCLUSÂO DE USUÁRIO
                         </h5>
                         <button type="button" class="close"
                             data-dismiss="modal" aria-label="Close">
